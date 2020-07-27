084b01151c11538cc59f9611e350aadff4deea6a
Tobias <tpersson@cern.ch>
2020-07-27 23:21:38 +0200

    Most works now but not all.

--- testing/updateFromList.py
+++ testing/updateFromList.py
@@ -31,7 +31,7 @@ from shutil import copyfile
 
 #copyfile(src, dst)
 mypath_orig = '../tests/'
-file1 = open('../failedTest.out', 'r') 
+file1 = open('../myFail.txt', 'r') 
 Lines = file1.readlines() 
 loc = os.getcwd()
 count = 0
